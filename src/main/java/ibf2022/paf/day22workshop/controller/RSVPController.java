9a93fedbbb79a6b53852e06922132a32f1baba60
Woody3797 <woody3797@gmail.com>
2023-04-04 09:59:09 +0800

    day22workshop

---
+++ src/main/java/ibf2022/paf/day22workshop/controller/RSVPController.java
@@ -0,0 +1,8 @@
+package ibf2022.paf.day22workshop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RSVPController {
+    
+}
---
+++ src/main/java/ibf2022/paf/day22workshop/model/Guest.java
@@ -0,0 +1,13 @@
+package ibf2022.paf.day22workshop.model;
+
+public class Guest {
+    
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private Date confirmDate;
+    private String comments;
+
+
+}
